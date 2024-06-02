be02185344b7a84f101da68d31bc7543bf63c267
Andres Orav <andres.orav@gmail.com>
2024-06-02 18:38:06 +0300

    Add indexOf() and tests

--- src/__tests__/array.algo.test.ts
+++ src/__tests__/array.algo.test.ts
@@ -2,120 +2,156 @@ import { ArrayAlgorithm } from '../algorithms/array.algo';
 import { Utils } from '../utils';
 
 describe('Array algorithms', () => {
-    const arrayAlgorithm = new ArrayAlgorithm();
-    const utils = new Utils();
+  const arrayAlgorithm = new ArrayAlgorithm();
+  const utils = new Utils();
 
-    /**
-     * Mutator Methods
-     *
-     * Mutator methods modify the array in place, altering its contents directly.
-     */
-    describe('Mutator Methods', () => {
-        test('push', () => {
-            const books = ['The Power of Now', 'Meditations'];
-            const newBook = 'Inner Engineering';
-            const expectedBooks = [...books, newBook]
+  /**
+   * Mutator Methods
+   *
+   * Mutator methods modify the array in place, altering its contents directly.
+   */
+  describe('Mutator Methods', () => {
+    test('push', () => {
+      const books = ['The Power of Now', 'Meditations'];
+      const newBook = 'Inner Engineering';
+      const expectedBooks = [...books, newBook];
 
-            expect(arrayAlgorithm.push(books, newBook)).toEqual(expectedBooks);
-        });
-
-        test('pop', () => {
-            const todo = 'do laundry';
-            const todos = ['buy milk', 'clean house', todo];
-            const expectedTodos = todos.slice(0, -1);
-            const [newTodos, removedTodo] = arrayAlgorithm.pop(todos);
-
-            expect(newTodos).toEqual(expectedTodos);
-            expect(removedTodo).toEqual(todo);
-        });
-
-        test('shift', () => {
-            const car = 'BMW';
-            const cars = [car, 'Tesla', 'KIA'];
-            const expectedCars = cars.slice(1);
-            const [newCars, removedCar] = arrayAlgorithm.shift(cars);
-
-            expect(newCars).toEqual(expectedCars);
-            expect(removedCar).toEqual(car);
-        });
-
-        test('unshift', () => {
-            const movie = 'Pitch Black';
-            const movies = ['Riddick', 'The Chronicles of Riddick'];
-            const expectedMovies = [movie, ...movies];
-
-            expect(arrayAlgorithm.unshift(movies, movie)).toEqual(expectedMovies);
-        });
-
-        describe('splice', () => {
-            const months = ['January', 'February', 'March', 'April'];
-
-            test('adds', () => {
-                const december = 'December';
-                const expectedMonths = [december, ...months];
-
-                expect(arrayAlgorithm.splice([...months], 0, 0, december)).toEqual(expectedMonths);
-            });
-
-            test('removes', () => {
-                expect(arrayAlgorithm.splice([...months, 'May'], months.length, 1)).toEqual(months);
-            });
-
-            test('replaces', () => {
-                const june = 'June';
-                const expectedMonths = [...months.slice(0, -1), june];
-
-                expect(arrayAlgorithm.splice([...months], months.length - 1, 1, june)).toEqual(expectedMonths);
-            });
-        });
-
-        test('reverse', () => {
-            const days = ['Monday', 'Tuesday', 'Wednesday'];
-            const expectedDays = days.slice().reverse();
-
-            expect(arrayAlgorithm.reverse(days)).toEqual(expectedDays);
-        });
-
-        describe('sort', () => {
-            test('names', () => {
-                const sortedNames = ['Anny', 'Beth', 'Chris', 'Dan', 'Ed'];
-                const shuffledNames = utils.shuffle(sortedNames);
-
-                expect(arrayAlgorithm.sort(shuffledNames)).toEqual(sortedNames);
-            });
-
-            test('numbers', () => {
-                const sortedNumbers = [1, 2, 2000, 3, 30, 31, 351];
-                const shuffledNumbers = utils.shuffle(sortedNumbers);
-
-                expect(arrayAlgorithm.sort(shuffledNumbers)).toEqual(sortedNumbers);
-            });
-        });
+      expect(arrayAlgorithm.push(books, newBook)).toEqual(expectedBooks);
     });
 
-    /**
-     * Accessor Methods
-     *
-     * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
-     */
-    describe('Accessor Methods', () => {
-        test('concat', () => {
-            const earlyYears = [2001, 2002, 2003, 2004];
-            const midYears = [2021, 2022, 2023, 2024];
-            const futureYears = [2045, 2046, 2047];
-            const allYearsCombined = [...earlyYears, ...midYears, ...futureYears]
+    test('pop', () => {
+      const todo = 'do laundry';
+      const todos = ['buy milk', 'clean house', todo];
+      const expectedTodos = todos.slice(0, -1);
+      const [newTodos, removedTodo] = arrayAlgorithm.pop(todos);
 
-            expect(arrayAlgorithm.concat(earlyYears, midYears, futureYears)).toEqual(allYearsCombined);
-        });
-
-        test('slice', () => {
-            const start = 1; // inclusive
-            const end = 3; // exclusive
-
-            const games = ['Fallout', 'Diablo', 'Star Craft', 'Tetris', 'Quake'];
-            const expectedGames = ['Diablo', 'Star Craft'];
-
-            expect(arrayAlgorithm.slice(games, start, end)).toEqual(expectedGames);
-        });
+      expect(newTodos).toEqual(expectedTodos);
+      expect(removedTodo).toEqual(todo);
     });
+
+    test('shift', () => {
+      const car = 'BMW';
+      const cars = [car, 'Tesla', 'KIA'];
+      const expectedCars = cars.slice(1);
+      const [newCars, removedCar] = arrayAlgorithm.shift(cars);
+
+      expect(newCars).toEqual(expectedCars);
+      expect(removedCar).toEqual(car);
+    });
+
+    test('unshift', () => {
+      const movie = 'Pitch Black';
+      const movies = ['Riddick', 'The Chronicles of Riddick'];
+      const expectedMovies = [movie, ...movies];
+
+      expect(arrayAlgorithm.unshift(movies, movie)).toEqual(expectedMovies);
+    });
+
+    describe('splice', () => {
+      const months = ['January', 'February', 'March', 'April'];
+
+      test('adds', () => {
+        const december = 'December';
+        const expectedMonths = [december, ...months];
+
+        expect(arrayAlgorithm.splice([...months], 0, 0, december)).toEqual(
+          expectedMonths,
+        );
+      });
+
+      test('removes', () => {
+        expect(
+          arrayAlgorithm.splice([...months, 'May'], months.length, 1),
+        ).toEqual(months);
+      });
+
+      test('replaces', () => {
+        const june = 'June';
+        const expectedMonths = [...months.slice(0, -1), june];
+
+        expect(
+          arrayAlgorithm.splice([...months], months.length - 1, 1, june),
+        ).toEqual(expectedMonths);
+      });
+    });
+
+    test('reverse', () => {
+      const days = ['Monday', 'Tuesday', 'Wednesday'];
+      const expectedDays = days.slice().reverse();
+
+      expect(arrayAlgorithm.reverse(days)).toEqual(expectedDays);
+    });
+
+    describe('sort', () => {
+      test('names', () => {
+        const sortedNames = ['Anny', 'Beth', 'Chris', 'Dan', 'Ed'];
+        const shuffledNames = utils.shuffle(sortedNames);
+
+        expect(arrayAlgorithm.sort(shuffledNames)).toEqual(sortedNames);
+      });
+
+      test('numbers', () => {
+        const sortedNumbers = [1, 2, 2000, 3, 30, 31, 351];
+        const shuffledNumbers = utils.shuffle(sortedNumbers);
+
+        expect(arrayAlgorithm.sort(shuffledNumbers)).toEqual(sortedNumbers);
+      });
+    });
+  });
+
+  /**
+   * Accessor Methods
+   *
+   * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
+   */
+  describe('Accessor Methods', () => {
+    test('concat', () => {
+      const earlyYears = [2001, 2002, 2003, 2004];
+      const midYears = [2021, 2022, 2023, 2024];
+      const futureYears = [2045, 2046, 2047];
+      const allYearsCombined = [...earlyYears, ...midYears, ...futureYears];
+
+      expect(arrayAlgorithm.concat(earlyYears, midYears, futureYears)).toEqual(
+        allYearsCombined,
+      );
+    });
+
+    test('slice', () => {
+      const start = 1; // inclusive
+      const end = 3; // exclusive
+
+      const games = ['Fallout', 'Diablo', 'Star Craft', 'Tetris', 'Quake'];
+      const expectedGames = ['Diablo', 'Star Craft'];
+
+      expect(arrayAlgorithm.slice(games, start, end)).toEqual(expectedGames);
+    });
+
+    describe('indexOf', () => {
+      const words = ['Sunshine', 'Breeze', 'Sunshine'];
+
+      test('first occurrence', () => {
+        const word = 'Sunshine';
+        const expectedIndex = 0;
+
+        expect(arrayAlgorithm.indexOf(words, word)).toEqual(expectedIndex);
+      });
+
+      test('second occurrence', () => {
+        const word = 'Sunshine';
+        const start = 1;
+        const expectedIndex = 2;
+
+        expect(arrayAlgorithm.indexOf(words, word, start)).toEqual(
+          expectedIndex,
+        );
+      });
+
+      test('not found', () => {
+        const word = 'Snow';
+        const expectedIndex = -1;
+
+        expect(arrayAlgorithm.indexOf(words, word)).toEqual(expectedIndex);
+      });
+    });
+  });
 });
--- src/algorithms/array.algo.ts
+++ src/algorithms/array.algo.ts
@@ -1,182 +1,167 @@
 import { BaseAlgorithm } from './baseAlgorithm';
 
 export class ArrayAlgorithm extends BaseAlgorithm {
-    /**
-     * Mutator Methods
-     *
-     * Mutator methods modify the array in place, altering its contents directly.
-     */
+  /**
+   * Mutator Methods
+   *
+   * Mutator methods modify the array in place, altering its contents directly.
+   */
 
-    /**
-     * Adds one or more elements to the end of an array
-     *
-     * @param arr - The array to which the element will be added.
-     * @param elem - The element to be added to the array.
-     * @returns The array with the new element added to the end.
-     */
-    public push<T>(arr: Array<T>, elem: T): Array<T> {
-        arr.push(elem);
-        return arr;
-    }
+  /**
+   * Adds one or more elements to the end of an array
+   *
+   * @param arr - The array to which the element will be added.
+   * @param elem - The element to be added to the array.
+   * @returns The array with the new element added to the end.
+   */
+  public push<T>(arr: Array<T>, elem: T): Array<T> {
+    arr.push(elem);
+    return arr;
+  }
 
-    /**
-     * Removes the last element from an array and returns the modified array and the removed element.
-     *
-     * @param arr - The array from which the last element will be removed.
-     * @returns A tuple where the first element is the modified array and the second element is the removed element.
-     */
-    public pop<T>(arr: Array<T>): [Array<T>, T | undefined] {
-        const removed = arr.pop();
-        return [arr, removed];
-    }
+  /**
+   * Removes the last element from an array and returns the modified array and the removed element.
+   *
+   * @param arr - The array from which the last element will be removed.
+   * @returns A tuple where the first element is the modified array and the second element is the removed element.
+   */
+  public pop<T>(arr: Array<T>): [Array<T>, T | undefined] {
+    const removed = arr.pop();
+    return [arr, removed];
+  }
 
-    /**
-     * Removes the first element from an array and returns the modified array and the removed element.
-     *
-     * @param arr - The array from which the first element will be removed.
-     * @returns A tuple where the first element is the modified array and the second element is the removed element.
-     */
-    public shift<T>(arr: Array<T>): [Array<T>, T | undefined] {
-        const removed = arr.shift();
-        return [arr, removed];
-    }
+  /**
+   * Removes the first element from an array and returns the modified array and the removed element.
+   *
+   * @param arr - The array from which the first element will be removed.
+   * @returns A tuple where the first element is the modified array and the second element is the removed element.
+   */
+  public shift<T>(arr: Array<T>): [Array<T>, T | undefined] {
+    const removed = arr.shift();
+    return [arr, removed];
+  }
 
-    /**
-     * Adds one or more elements to the beginning of an array and returns the modified array.
-     *
-     * @param arr - The array to which the element will be added.
-     * @param elem - The element to be added to the beginning of the array.
-     * @returns The array with the new element added to the beginning.
-     */
-    public unshift<T>(arr: Array<T>, elem: T): Array<T> {
-        arr.unshift(elem);
-        return arr;
-    }
+  /**
+   * Adds one or more elements to the beginning of an array and returns the modified array.
+   *
+   * @param arr - The array to which the element will be added.
+   * @param elem - The element to be added to the beginning of the array.
+   * @returns The array with the new element added to the beginning.
+   */
+  public unshift<T>(arr: Array<T>, elem: T): Array<T> {
+    arr.unshift(elem);
+    return arr;
+  }
 
-    /**
-     * Changes the contents of an array by removing, replacing, or adding new elements at a specific index.
-     *
-     * @param arr - The array to be modified.
-     * @param start - The index at which to start changing the array.
-     * @param deleteCount - The number of elements in the array to remove from start.
-     * @param items - The elements to add to the array, beginning from start. If you don't specify any elements, splice() will only remove elements from the array.
-     * @returns The array with the new elements added, replaced, or removed.
-     */
-    public splice<T>(arr: Array<T>, start: number, deleteCount: number, ...items: T[]): Array<T> {
-        arr.splice(start, deleteCount, ...items);
-        return arr;
-    }
+  /**
+   * Changes the contents of an array by removing, replacing, or adding new elements at a specific index.
+   *
+   * @param arr - The array to be modified.
+   * @param start - The index at which to start changing the array.
+   * @param deleteCount - The number of elements in the array to remove from start.
+   * @param items - The elements to add to the array, beginning from start. If you don't specify any elements, splice() will only remove elements from the array.
+   * @returns The array with the new elements added, replaced, or removed.
+   */
+  public splice<T>(
+    arr: Array<T>,
+    start: number,
+    deleteCount: number,
+    ...items: T[]
+  ): Array<T> {
+    arr.splice(start, deleteCount, ...items);
+    return arr;
+  }
 
-    /**
-     * Reverses the array in place
-     *
-     * @param arr - The array to be reversed.
-     * @returns The reversed array.
-     */
-    public reverse<T>(arr: Array<T>): Array<T> {
-        arr.reverse();
-        return arr;
-    }
+  /**
+   * Reverses the array in place
+   *
+   * @param arr - The array to be reversed.
+   * @returns The reversed array.
+   */
+  public reverse<T>(arr: Array<T>): Array<T> {
+    arr.reverse();
+    return arr;
+  }
 
-    /**
-     * Sorts the elements of an array in place
-     *
-     * @param arr - The array to be sorted.
-     * @returns The sorted array.
-     */
-    public sort<T>(arr: Array<T>): Array<T> {
-        arr.sort();
-        return arr;
-    }
+  /**
+   * Sorts the elements of an array in place
+   *
+   * @param arr - The array to be sorted.
+   * @returns The sorted array.
+   */
+  public sort<T>(arr: Array<T>): Array<T> {
+    arr.sort();
+    return arr;
+  }
 
-    /**
-     * Accessor Methods
-     *
-     * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
-     */
+  /**
+   * Accessor Methods
+   *
+   * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
+   */
 
-    /**
-     * Merges two or more arrays into a new array.
-     *
-     * @param arr - The initial array.
-     * @param arrays - Additional arrays to be merged.
-     * @returns A new array that contains all elements from the initial array and the additional arrays.
-     */
-    public concat<T>(arr: Array<T>, ...arrays: Array<Array<T>>): Array<T> {
-        return arr.concat(...arrays);
-    }
+  /**
+   * Merges two or more arrays into a new array.
+   *
+   * @param arr - The initial array.
+   * @param arrays - Additional arrays to be merged.
+   * @returns A new array that contains all elements from the initial array and the additional arrays.
+   */
+  public concat<T>(arr: Array<T>, ...arrays: Array<Array<T>>): Array<T> {
+    return arr.concat(...arrays);
+  }
 
-    /**
-     * Returns a shallow copy of a portion of an array.
-     *
-     * @param arr - The array to be sliced.
-     * @param start - The beginning index, inclusive.
-     * @param end - The end index, exclusive.
-     * @returns A new array that is a shallow copy of the specified portion of the original array.
-     */
-    public slice<T>(arr: Array<T>, start: number, end: number): Array<T> {
-        return arr.slice(start, end);
-    }
+  /**
+   * Returns a shallow copy of a portion of an array.
+   *
+   * @param arr - The array to be sliced.
+   * @param start - The beginning index, inclusive.
+   * @param end - The end index, exclusive.
+   * @returns A new array that is a shallow copy of the specified portion of the original array.
+   */
+  public slice<T>(arr: Array<T>, start: number, end: number): Array<T> {
+    return arr.slice(start, end);
+  }
 
-    public indexOf(): void {
+  /**
+   * Returns the first index at which a given element can be found in the array.
+   * @param arr - The array in which to search for the element.
+   * @param elem - The element to search for.
+   * @param start - The index to start the search from. If not provided, the search starts from the beginning of the array.
+   * @returns The first index at which the given element can be found in the array, or -1 if the element is not found.
+   */
+  public indexOf<T>(arr: Array<T>, elem: T, start?: number): number {
+    return arr.indexOf(elem, start);
+  }
 
-    }
+  public lastIndexOf(): void {}
 
-    public lastIndexOf(): void {
+  public includes(): void {}
 
-    }
+  public join(): void {}
 
-    public includes(): void {
+  public toString(): void {}
 
-    }
+  /**
+   * Iteration Methods
+   *
+   * Iteration methods process each element of an array in some manner, usually returning a new value or array based on the original array.
+   */
+  public forEach(): void {}
 
-    public join(): void {
+  public map(): void {}
 
-    }
+  public filter(): void {}
 
-    public toString(): void {
+  public reduce(): void {}
 
-    }
+  public reduceRight(): void {}
 
-    /**
-     * Iteration Methods
-     *
-     * Iteration methods process each element of an array in some manner, usually returning a new value or array based on the original array.
-     */
-    public forEach(): void {
+  public some(): void {}
 
-    }
+  public every(): void {}
 
-    public map(): void {
+  public find(): void {}
 
-    }
-
-    public filter(): void {
-
-    }
-
-    public reduce(): void {
-
-    }
-
-    public reduceRight(): void {
-
-    }
-
-    public some(): void {
-
-    }
-
-    public every(): void {
-
-    }
-
-
-    public find(): void {
-
-    }
-
-    public findIndex(): void {
-
-    }
+  public findIndex(): void {}
 }

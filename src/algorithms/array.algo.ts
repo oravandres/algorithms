bb2a6d8acbab0d71bc113001e1ee4fa385d1c4da
Andres Orav <andres.orav@gmail.com>
2024-06-02 19:11:58 +0300

    Add lastIndexOf() and tests
    Also fixes prettier issues

--- src/algorithms/array.algo.ts
+++ src/algorithms/array.algo.ts
@@ -1,168 +1,187 @@
 import { BaseAlgorithm } from './baseAlgorithm';
 
 export class ArrayAlgorithm extends BaseAlgorithm {
-  /**
-   * Mutator Methods
-   *
-   * Mutator methods modify the array in place, altering its contents directly.
-   */
+    /**
+     * Mutator Methods
+     *
+     * Mutator methods modify the array in place, altering its contents directly.
+     */
 
-  /**
-   * Adds one or more elements to the end of an array
-   *
-   * @param arr - The array to which the element will be added.
-   * @param elem - The element to be added to the array.
-   * @returns The array with the new element added to the end.
-   */
-  public push<T>(arr: Array<T>, elem: T): Array<T> {
-    arr.push(elem);
-    return arr;
-  }
+    /**
+     * Adds one or more elements to the end of an array
+     *
+     * @param arr - The array to which the element will be added.
+     * @param elem - The element to be added to the array.
+     * @returns The array with the new element added to the end.
+     */
+    public push<T>(arr: Array<T>, elem: T): Array<T> {
+        arr.push(elem);
+        return arr;
+    }
 
-  /**
-   * Removes the last element from an array and returns the modified array and the removed element.
-   *
-   * @param arr - The array from which the last element will be removed.
-   * @returns A tuple where the first element is the modified array and the second element is the removed element.
-   */
-  public pop<T>(arr: Array<T>): [Array<T>, T | undefined] {
-    const removed = arr.pop();
-    return [arr, removed];
-  }
+    /**
+     * Removes the last element from an array and returns the modified array and the removed element.
+     *
+     * @param arr - The array from which the last element will be removed.
+     * @returns A tuple where the first element is the modified array and the second element is the removed element.
+     */
+    public pop<T>(arr: Array<T>): [Array<T>, T | undefined] {
+        const removed = arr.pop();
+        return [arr, removed];
+    }
 
-  /**
-   * Removes the first element from an array and returns the modified array and the removed element.
-   *
-   * @param arr - The array from which the first element will be removed.
-   * @returns A tuple where the first element is the modified array and the second element is the removed element.
-   */
-  public shift<T>(arr: Array<T>): [Array<T>, T | undefined] {
-    const removed = arr.shift();
-    return [arr, removed];
-  }
+    /**
+     * Removes the first element from an array and returns the modified array and the removed element.
+     *
+     * @param arr - The array from which the first element will be removed.
+     * @returns A tuple where the first element is the modified array and the second element is the removed element.
+     */
+    public shift<T>(arr: Array<T>): [Array<T>, T | undefined] {
+        const removed = arr.shift();
+        return [arr, removed];
+    }
 
-  /**
-   * Adds one or more elements to the beginning of an array and returns the modified array.
-   *
-   * @param arr - The array to which the element will be added.
-   * @param elem - The element to be added to the beginning of the array.
-   * @returns The array with the new element added to the beginning.
-   */
-  public unshift<T>(arr: Array<T>, elem: T): Array<T> {
-    arr.unshift(elem);
-    return arr;
-  }
+    /**
+     * Adds one or more elements to the beginning of an array and returns the modified array.
+     *
+     * @param arr - The array to which the element will be added.
+     * @param elem - The element to be added to the beginning of the array.
+     * @returns The array with the new element added to the beginning.
+     */
+    public unshift<T>(arr: Array<T>, elem: T): Array<T> {
+        arr.unshift(elem);
+        return arr;
+    }
 
-  /**
-   * Changes the contents of an array by removing, replacing, or adding new elements at a specific index.
-   *
-   * @param arr - The array to be modified.
-   * @param start - The index at which to start changing the array.
-   * @param deleteCount - The number of elements in the array to remove from start.
-   * @param items - The elements to add to the array, beginning from start. If you don't specify any elements, splice() will only remove elements from the array.
-   * @returns The array with the new elements added, replaced, or removed.
-   */
-  public splice<T>(
-    arr: Array<T>,
-    start: number,
-    deleteCount: number,
-    ...items: T[]
-  ): Array<T> {
-    arr.splice(start, deleteCount, ...items);
-    return arr;
-  }
+    /**
+     * Changes the contents of an array by removing, replacing, or adding new elements at a specific index.
+     *
+     * @param arr - The array to be modified.
+     * @param start - The index at which to start changing the array.
+     * @param deleteCount - The number of elements in the array to remove from start.
+     * @param items - The elements to add to the array, beginning from start. If you don't specify any elements, splice() will only remove elements from the array.
+     * @returns The array with the new elements added, replaced, or removed.
+     */
+    public splice<T>(
+        arr: Array<T>,
+        start: number,
+        deleteCount: number,
+        ...items: T[]
+    ): Array<T> {
+        arr.splice(start, deleteCount, ...items);
+        return arr;
+    }
 
-  /**
-   * Reverses the array in place
-   *
-   * @param arr - The array to be reversed.
-   * @returns The reversed array.
-   */
-  public reverse<T>(arr: Array<T>): Array<T> {
-    arr.reverse();
-    return arr;
-  }
+    /**
+     * Reverses the array in place
+     *
+     * @param arr - The array to be reversed.
+     * @returns The reversed array.
+     */
+    public reverse<T>(arr: Array<T>): Array<T> {
+        arr.reverse();
+        return arr;
+    }
 
-  /**
-   * Sorts the elements of an array in place
-   *
-   * @param arr - The array to be sorted.
-   * @returns The sorted array.
-   */
-  public sort<T>(arr: Array<T>): Array<T> {
-    arr.sort();
-    return arr;
-  }
+    /**
+     * Sorts the elements of an array in place
+     *
+     * @param arr - The array to be sorted.
+     * @returns The sorted array.
+     */
+    public sort<T>(arr: Array<T>): Array<T> {
+        arr.sort();
+        return arr;
+    }
 
-  /**
-   * Accessor Methods
-   *
-   * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
-   */
+    /**
+     * Accessor Methods
+     *
+     * Accessor methods do not modify the original array but return some derived or specific value based on the array's content.
+     */
 
-  /**
-   * Merges two or more arrays into a new array.
-   *
-   * @param arr - The initial array.
-   * @param arrays - Additional arrays to be merged.
-   * @returns A new array that contains all elements from the initial array and the additional arrays.
-   */
-  public concat<T>(arr: Array<T>, ...arrays: Array<Array<T>>): Array<T> {
-    return arr.concat(...arrays);
-  }
+    /**
+     * Merges two or more arrays into a new array.
+     *
+     * @param arr - The initial array.
+     * @param arrays - Additional arrays to be merged.
+     * @returns A new array that contains all elements from the initial array and the additional arrays.
+     */
+    public concat<T>(arr: Array<T>, ...arrays: Array<Array<T>>): Array<T> {
+        return arr.concat(...arrays);
+    }
 
-  /**
-   * Returns a shallow copy of a portion of an array.
-   *
-   * @param arr - The array to be sliced.
-   * @param start - The beginning index, inclusive.
-   * @param end - The end index, exclusive.
-   * @returns A new array that is a shallow copy of the specified portion of the original array.
-   */
-  public slice<T>(arr: Array<T>, start: number, end: number): Array<T> {
-    return arr.slice(start, end);
-  }
+    /**
+     * Returns a shallow copy of a portion of an array.
+     *
+     * @param arr - The array to be sliced.
+     * @param start - The beginning index, inclusive.
+     * @param end - The end index, exclusive.
+     * @returns A new array that is a shallow copy of the specified portion of the original array.
+     */
+    public slice<T>(arr: Array<T>, start: number, end: number): Array<T> {
+        return arr.slice(start, end);
+    }
 
-  /**
-   * Returns the first index at which a given element can be found in the array.
-   *
-   * @param arr - The array in which to search for the element.
-   * @param elem - The element to search for.
-   * @param start - The index to start the search from. If not provided, the search starts from the beginning of the array.
-   * @returns The first index at which the given element can be found in the array, or -1 if the element is not found.
-   */
-  public indexOf<T>(arr: Array<T>, elem: T, start?: number): number {
-    return arr.indexOf(elem, start);
-  }
+    /**
+     * Returns the first index at which a given element can be found in the array.
+     *
+     * @param arr - The array in which to search for the element.
+     * @param elem - The element to search for.
+     * @param start - The index to start the search from. If not provided, the search starts from the beginning of the array.
+     * @returns The first index at which the given element can be found in the array, or -1 if the element is not found.
+     */
+    public indexOf<T>(arr: Array<T>, elem: T, start?: number): number {
+        return arr.indexOf(elem, start);
+    }
 
-  public lastIndexOf(): void {}
+    /**
+     * Returns the last index at which a given element can be found in the array.
+     * The array is searched backwards, starting from the 'start' index towards the beginning of the array.
+     * If 'start' is undefined, the search starts from the end of the array.
+     *
+     * @param arr - The array in which to search for the element.
+     * @param elem - The element to search for.
+     * @param start - The index to start the search from. If not provided, the search starts from the end of the array.
+     * @returns The last index at which the given element can be found in the array, or -1 if the element is not found.
+     */
+    public lastIndexOf<T>(arr: Array<T>, elem: T, start?: number): number {
+        // If 'start' is undefined, set it to the last index of the array.
+        // This is a workaround for a known issue in Node.js where passing 'undefined' as the 'start' parameter
+        // to 'Array.prototype.lastIndexOf' starts the search from the beginning of the array instead of the end.
+        if (start === undefined) {
+            start = arr.length - 1;
+        }
 
-  public includes(): void {}
+        return arr.lastIndexOf(elem, start);
+    }
 
-  public join(): void {}
+    public includes(): void {}
 
-  public toString(): void {}
+    public join(): void {}
 
-  /**
-   * Iteration Methods
-   *
-   * Iteration methods process each element of an array in some manner, usually returning a new value or array based on the original array.
-   */
-  public forEach(): void {}
+    public toString(): void {}
 
-  public map(): void {}
+    /**
+     * Iteration Methods
+     *
+     * Iteration methods process each element of an array in some manner, usually returning a new value or array based on the original array.
+     */
+    public forEach(): void {}
 
-  public filter(): void {}
+    public map(): void {}
 
-  public reduce(): void {}
+    public filter(): void {}
 
-  public reduceRight(): void {}
+    public reduce(): void {}
 
-  public some(): void {}
+    public reduceRight(): void {}
 
-  public every(): void {}
+    public some(): void {}
 
-  public find(): void {}
+    public every(): void {}
 
-  public findIndex(): void {}
+    public find(): void {}
+
+    public findIndex(): void {}
 }

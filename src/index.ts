3c5a69be281f3d029c5d49b9bd5784569aae8a58
Andres Orav <andres.orav@gmail.com>
2024-06-02 20:10:38 +0300

    Small readability improvements

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ const algorithmsDir = path.join(__dirname, 'algorithms');
 fs.readdirSync(algorithmsDir).forEach(file => {
     if (file.endsWith('.js') && !file.endsWith('baseAlgorithm.js')) {
         const filePath = path.join(algorithmsDir, file);
+
         import(filePath).then(module => {
             const className = Object.keys(module)[0];
             const AlgorithmClass = module[className];
             const algorithmInstance = new AlgorithmClass();
+
             if (typeof algorithmInstance.description === 'function') {
                 console.log(algorithmInstance.description());
             }
